57f6d31a7aa68f2168f286a7527767cbedf390d4
Aaron Davis <aaron.e.davis@gmail.com>
2021-10-14 17:14:04 -0600

    minor updates

--- solarstat.py
+++ solarstat.py
@@ -3,29 +3,31 @@ import requests
 import json
 import datetime
 
-config = configparser.ConfigParser()
-config.read("config.ini")
-key = config["DEFAULT"]["key"]
-user = config["DEFAULT"]["user_id"]
-system = config["DEFAULT"]["system"]
+if __name__ == "__main__":
 
-url = (
-    "https://api.enphaseenergy.com/api/v2/systems/"
-    + system
-    + "/summary?key="
-    + key
-    + "&user_id="
-    + user
-)
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    key = config["DEFAULT"]["key"]
+    user = config["DEFAULT"]["user_id"]
+    system = config["DEFAULT"]["system"]
 
-payload = {}
-headers = {}
+    url = (
+        "https://api.enphaseenergy.com/api/v2/systems/"
+        + system
+        + "/summary?key="
+        + key
+        + "&user_id="
+        + user
+    )
 
-response = requests.request("GET", url, headers=headers, data=payload)
-respjson = json.loads(response.text)
-lastreport = datetime.datetime.fromtimestamp(int(respjson["last_report_at"]))
-status = respjson["status"]
+    payload = {}
+    headers = {}
 
-print(f"Energy collected today: {(respjson['energy_today'])}")
-print(f"Last reported on {lastreport}")
-print(f"Solar array status is '{status}'.")
+    response = requests.request("GET", url, headers=headers, data=payload)
+    respjson = json.loads(response.text)
+    lastreport = datetime.datetime.fromtimestamp(int(respjson["last_report_at"]))
+    status = respjson["status"]
+
+    print(f"Energy collected today: {(respjson['energy_today'])}")
+    print(f"Last reported on {lastreport}")
+    print(f"Solar array status is '{status}'.")
