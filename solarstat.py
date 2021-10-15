f0c4186df33bf86d6fd4b5d2331a6614a1c47305
Aaron Davis <aaron.e.davis@gmail.com>
2021-10-14 22:07:13 -0600

    Added MongoDB linkage

--- solarstat.py
+++ solarstat.py
@@ -2,6 +2,10 @@ import configparser
 import requests
 import json
 import datetime
+import certifi
+import pymongo
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 if __name__ == "__main__":
 
@@ -10,6 +14,13 @@ if __name__ == "__main__":
     key = config["DEFAULT"]["key"]
     user = config["DEFAULT"]["user_id"]
     system = config["DEFAULT"]["system"]
+    mongoaddr = config["MONGO"]["mongo_addr"]
+    mongodb = config["MONGO"]["mongo_db"]
+    mongocollect = config["MONGO"]["mongo_collect"]
+    mongouser = config["MONGO"]["user_name"]
+    mongopw = config["MONGO"]["password"]
+
+    maxMongoDBDelay = 500
 
     url = (
         "https://api.enphaseenergy.com/api/v2/systems/"
@@ -20,6 +31,23 @@ if __name__ == "__main__":
         + user
     )
 
+    client = MongoClient(
+        "mongodb+srv://"
+        + mongouser
+        + ":"
+        + mongopw
+        + "@"
+        + mongoaddr
+        + "/"
+        + mongodb
+        + "?retryWrites=true&w=majority",
+        tlsCAFile=certifi.where(),
+        serverSelectionTimeoutMS=maxMongoDBDelay,
+    )
+
+    db = client[mongodb]
+    collection = db[mongocollect]
+
     payload = {}
     headers = {}
 
@@ -27,7 +55,27 @@ if __name__ == "__main__":
     respjson = json.loads(response.text)
     lastreport = datetime.datetime.fromtimestamp(int(respjson["last_report_at"]))
     status = respjson["status"]
+    collected = respjson["energy_today"]
 
-    print(f"Energy collected today: {(respjson['energy_today'])}")
+    print(f"Energy collected today: {collected}")
     print(f"Last reported on {lastreport}")
     print(f"Solar array status is '{status}'.")
+
+    try:
+        client.admin.command("ping")
+    except ConnectionFailure:
+        print("Server not available")
+
+    """try:
+        for coll in client.getCollectionNames():
+            print(f"default collection: {coll}")
+    except ConnectionFailure:
+        print("Database not available")
+    """
+
+    try:
+        insert = {"LastReported": lastreport, "Collected": collected, "Status": status}
+        post = collection.insert_one(insert)
+        print("Created record as {0}".format(post.inserted_id))
+    except pymongo.errors.ServerSelectionTimeoutError as err:
+        print(err)
