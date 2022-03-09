0bf3cc859d2aafd9012424ebc17b3701780e2c2c
Aaron Davis <aaron.e.davis@gmail.com>
2022-03-09 16:37:10 -0700

    Updated copyright date

--- solarstat.py
+++ solarstat.py
@@ -3,7 +3,7 @@
 
 __author__ = "Aaron Davis"
 __version__ = "0.1.0"
-__copyright__ = "Copyright (c) 2021 Aaron Davis"
+__copyright__ = "Copyright (c) 2022 Aaron Davis"
 __license__ = "MIT License"
 
 import configparser
